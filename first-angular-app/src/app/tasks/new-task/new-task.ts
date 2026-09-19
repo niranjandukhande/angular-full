f0eade0ebabcd8c7f4f15448e28ec9658883aec8
Niranjan Dukhande <progsolos@gmail.com>
2026-09-19 19:47:07 +0530

    handling form submission and using the submitted data

--- first-angular-app/src/app/tasks/new-task/new-task.ts
+++ first-angular-app/src/app/tasks/new-task/new-task.ts
@@ -1,15 +1,34 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { type NewTaskData } from '../task/task.model';
 
 @Component({
-  imports: [],
+  imports: [FormsModule],
   selector: 'app-new-task',
   styleUrl: './new-task.css',
   templateUrl: './new-task.html',
 })
 export class NewTask {
   @Output() cancel = new EventEmitter<void>();
+  @Output() add = new EventEmitter<NewTaskData>();
+
+  // enteredTitle = signal('');
+  // enteredSummary = signal('');
+  // enteredDate = signal('');
+
+  enteredTitle: string = '';
+  enteredSummary: string = '';
+  enteredDate: string = '';
 
   onCancel() {
     this.cancel.emit();
+  }
+
+  onSubmit() {
+    this.add.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      date: this.enteredDate,
+    });
   }
 }
--- first-angular-app/src/app/tasks/tasks.ts
+++ first-angular-app/src/app/tasks/tasks.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Task } from './task/task';
 import { NewTask } from './new-task/new-task';
+import { type NewTaskData } from './task/task.model';
 
 @Component({
   imports: [Task, NewTask],
@@ -53,6 +54,17 @@ export class Tasks {
   }
 
   onCancelAddTask() {
+    this.isAddingTask = false;
+  }
+
+  onAddTask(taskData: NewTaskData) {
+    this.tasks.unshift({
+      id: new Date().getTime().toString(),
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date,
+    });
     this.isAddingTask = false;
   }
 }
